74c911dc6e026861c3399117bda5ab3ccb42229d
GreenArt <GreenArt@DESKTOP-758VK1T>
2022-01-17 16:37:35 +0900

    파일의 확장자 정규식 넣은작업 01-17

--- SpringProject/src/main/webapp/resources/js/uploadAjax.js
+++ SpringProject/src/main/webapp/resources/js/uploadAjax.js
@@ -2,6 +2,33 @@
  * 
  */
 $(document).ready(function(){
+	
+	//파일의 크리와 화장자를 검사하는 함수 선언
+	//서버에서 설정해 놓은 파일크기 설정
+	var maxSize=5242880;  //5MB
+	//서버에서 허용 가능한 확장자 설정(정규식)
+	var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+	
+	function checkExtension(fileSize,fileName){
+		var str ="";
+		
+		//사용자가 선택한 파일의 크기가 서버에서 설정해 놓은 파일의 크기보다 이상이면,
+		if(fileSize>=maxSize){
+			//str =+"파일 사이즈 초과";
+			alert("파일 사이즈 초과");
+			return false;
+		}
+		//사용자가 선택한 파일의 확장자가 서버에서 설정해 놓은 파일의 확장자와 일치하지 않으면
+		if(regex.test(fileName)){
+			//str =+"해당 종류의 파일은 업로드 할 수 없습니다.";
+			alert("해당 종류의 파일은 업로드 할 수 없습니다.");
+			return false;
+		}
+		//alert(str);
+		return true;	
+	}
+	
+	
 	$("input[type='submit']").on("click",function(){
 		alert("클릭");
 		
@@ -11,6 +38,11 @@ $(document).ready(function(){
 		console.log(files);
 		
 		for(var i=0;i<files.length;i++){
+			//파일크기가 이상하면 밑에있는거 하지마라
+			if(!checkExtension(files[i].size,files[i].name)){
+				//밑에거 하지마라
+				return false;
+			}
 			formData.append("uploadFile",files[i]);
 		}
 		
@@ -22,9 +54,16 @@ $(document).ready(function(){
 			processData:false,
 			contentType:false,
 			success:function(result){
-				alert("upload성공")
-			}
+				//이게 실행될려면 ReplyController에 HttpSt ok가 실행될때 실행된다
+				alert("upload성공");
+			},   // 통신이 정상적으로 성공했으면
+	        error:function(){
+	            
+	        } 
 			
 		})
 	})
-})
+	
+	
+	
+})//ready end
