1a1072132ebaf437f23b8ebaac8c963cc9bc075b
GreenArt <GreenArt@DESKTOP-758VK1T>
2022-01-18 17:35:27 +0900

    이미지파일의 썸내일을 배열에 추가하여 웹페이지에서 값을 가져올수 있게 만들었다 최종점검

--- SpringProject/src/main/java/org/jun/controller/UploadController.java
+++ SpringProject/src/main/java/org/jun/controller/UploadController.java
@@ -7,9 +7,14 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
 
+import org.jun.domain.AttachFileDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -57,8 +62,13 @@ public class UploadController {
 		}//end for
 	}
 	//uplpadAjax.jsp에서 Ajax태그를 이용해서 파일 업로드 controller
-	@PostMapping("uploadAjaxAction")
-	public void uploadAjaxAction(MultipartFile[] uploadFile) {
+	@PostMapping(value="uploadAjaxAction",produces={MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ArrayList<AttachFileDTO>> uploadAjaxAction(MultipartFile[] uploadFile) {
+		// AttachFileDTO에 저장되는 값이 여러파일에 대한 값이면 배열로 처리가 되어야 하므로ArrayList타입이 되어야 함
+		ArrayList<AttachFileDTO> list = new ArrayList<>();
+		
+		
+		
 		//MultipartFile를 사용하면 DTO가 필요없다
 		System.out.println("MultipartFile = "+uploadFile[0]);
 		System.out.println("getOriginalFilename = "+uploadFile[0].getOriginalFilename()); //파일의 이름 컴활2급.jpg
@@ -70,10 +80,14 @@ public class UploadController {
 		
 		String uploadFolder="C:\\Users\\GreenArt\\git\\upload"; //파일 업로드하고자 하는 위치
 		
+
 		
+		String uplodFolderPath = getFolder();
 		//폴더생성                               (기존폴더,        현재폴더)를 결합
-		File uploadPath = new File(uploadFolder,getFolder());
-		System.out.println(uploadPath);
+		File uploadPath = new File(uploadFolder,uplodFolderPath);
+		System.out.println("uploadPath= "+uploadPath);
+		
+		
 		
 		//현재 만들려고 하는 폴더가 없으면
 		if(uploadPath.exists()==false) {
@@ -84,11 +98,24 @@ public class UploadController {
 		
 		
 		for(MultipartFile multiparFile : uploadFile) {
+			
+			//UploadController에 있는 uploadAjaxAction에서 AttachFileDTO을 사용해서 값을 저장해야 되는데
+			//이럴경우 UploadController에 AttachFileDTO가 없으면 사용 할 수가 없습니다
+			//그래서 UploadController에 AttachFileDTO 포함을 시켜서 사용하여 값을 저장함
+			AttachFileDTO attachdto = new AttachFileDTO();
+			
 			System.out.println("업로드 파일 이름 = "+multiparFile.getOriginalFilename()); //파일의 이름 컴활2급.jpg
 			System.out.println("업로드 파일 크기 = "+multiparFile.getSize());				//파일사이즈 197051
 			System.out.println("업로드 파일 형식 = "+multiparFile.getContentType());		//파일의타입 image/jpeg
 			
 			String uploadFileName=multiparFile.getOriginalFilename();
+			
+			attachdto.setUploadPath(uploadFolder);
+			
+			//실제 파일명 uploadFileName을 AttachFileDTO클래스(attachdto)에 fileName에 저장(setFileName)
+			attachdto.setFilename(uploadFileName);
+			
+			
 			//중복이 되지않는 임의의 문자열을 생성
 			UUID uuid = UUID.randomUUID();
 			
@@ -96,14 +123,26 @@ public class UploadController {
 			uploadFileName=uuid.toString()+"_"+uploadFileName;
 			
 			//uploadFolder에 저장되어 있는 경로로 실제 파일명으로 저장
-			File saveFile = new File(uploadFolder,uploadFileName); //파일은 기본생성자가 없다
+			File saveFile = new File(uploadPath,uploadFileName); //파일은 기본생성자가 없다
+			
+			
 			
 			//반드시 예외 처리를 해야한다 그냥은 못쓴다
 			try{
 				//saveFile변수에 저장되어 있는 폴더명으로 파일을 보내라
 				multiparFile.transferTo(saveFile);
+				
+				//실제 업로드 경로 (uplodFolderPath) 를 AttachFileDTO클래스(attachdto) 에 uplodPath에 저장(setFileName)
+				attachdto.setUploadPath(uplodFolderPath);
+				//uuid값(UUID) 를 AttachFileDTO클래스(attachdto) 에 uploadPath에 저장(uplodFolderPath)
+				attachdto.setUuid(uuid.toString());
+				
+				
 				//이미지 파일이면
 				if(checkImage(saveFile)) {
+					//FileType값을 AttachFileDTO클래스(attachdto) 에 uploadPath에 저장(uplodImage)
+					attachdto.setImage(true);
+					
 					//썸내일 파일을 생성하기 전에 썸네일 파일 이름을 추출
 					FileOutputStream thumnail = new FileOutputStream(new File(uploadPath,"s_"+uploadFileName));
 					//썸내일 파일 생성함
@@ -111,11 +150,17 @@ public class UploadController {
 					//썸내일 종료(메모리 공간 함수)
 					thumnail.close();
 				}
+				//list에 DTO값을 들어오게함
+				list.add(attachdto);
+				
 			}catch (Exception e) {
 				e.printStackTrace();
 			}//end try
-			
 		}//end for
+		
+		// 통신상태가 정상적이면 list에 저장되어 있는 값을 웹브라우저에 보내라   값을 uploadAjax.js에 있는 ajax에 success로 보내라
+		return  new ResponseEntity<>(list,HttpStatus.OK);
+		
 	}
 	//년/월/일 폴더의 생성
 	private String getFolder() {
---
+++ SpringProject/src/main/java/org/jun/domain/AttachFileDTO.java
@@ -0,0 +1,40 @@
+package org.jun.domain;
+
+public class AttachFileDTO {
+
+	private String filename;
+	private String uploadPath;
+	private String uuid;
+	private boolean image;
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	public String getUploadPath() {
+		return uploadPath;
+	}
+	public void setUploadPath(String uploadPath) {
+		this.uploadPath = uploadPath;
+	}
+	public String getUuid() {
+		return uuid;
+	}
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+	public boolean isImage() {
+		return image;
+	}
+	public void setImage(boolean image) {
+		this.image = image;
+	}
+	@Override
+	public String toString() {
+		return "AttachFileDTO [filename=" + filename + ", uploadPath=" + uploadPath + ", uuid=" + uuid + ", image="
+				+ image + "]";
+	}
+	
+	
+}
